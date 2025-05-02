bea051461f21166b2f0f2ea374705d3c6ab3ecb6
tyang816 <ty_ang@163.com>
2025-05-02 11:46:12 +0800

    Update BioLiP FASTA processing script to change base directory path

--- script/data/biolip/process_biolip_fasta.sh
+++ script/data/biolip/process_biolip_fasta.sh
@@ -1,4 +1,4 @@
-base_dir=data/BioLiP_202503
+base_dir=data/BioLiP_2503
 python src/data/biolip/process_biolip_fasta.py \
     --fasta_path ${base_dir}/cluster/BioLiP_clean100.fasta_rep_seq.fasta \
     --output_csv_path ${base_dir}/BioLiP_token_cls_100.csv
